a1283677270bba6bb620972884e0574a0b84a7f3
Wendel Nascimento <wendel.nascimento2@outlook.com>
2020-05-07 11:01:33 -0300

    updating dependencies and changing code style

--- index.js
+++ index.js
@@ -1,95 +1,89 @@
 #!/usr/bin/env node
 
-var fs       = require('fs');
-var os       = require('os');
-var path     = require('path');
-var chpro    = require('child_process');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const chpro = require('child_process');
 
-var through  = require('through');
-var csv      = require('csv-stream');
-var osenv    = require('osenv');
-var duplexer = require('duplexer');
-var concat   = require('concat-stream');
-var eventStream = require('event-stream');
+const csv = require('csv-stream');
+const osenv = require('osenv');
+const concat = require('concat-stream');
+const eventStream = require('event-stream');
 
-var spawn = chpro.spawn
-if (os.type() === 'Windows_NT') spawn = require('win-spawn')
+let { spawn } = chpro;
+if (os.type() === 'Windows_NT') spawn = require('cross-spawn');
 
 module.exports = function (options) {
+  const read = eventStream.through();
+  let duplex;
 
-  var read = through()
-  var duplex
+  const filename = path.join(osenv.tmpdir(), `_${Date.now()}`);
 
-  var filename = path.join(osenv.tmpdir(), '_' + Date.now())
-
-  var spawnArgs = []
+  const spawnArgs = [];
 
   if (options) {
-    options.sheet && spawnArgs.push('--sheet') && spawnArgs.push(options.sheet) && delete options.sheet
-    options.sheetIndex && spawnArgs.push('--sheet-index') && spawnArgs.push(options.sheetIndex) && delete options.sheetIndex
+    options.sheet && spawnArgs.push('--sheet') && spawnArgs.push(options.sheet) && delete options.sheet;
+    options.sheetIndex && spawnArgs.push('--sheet-index') && spawnArgs.push(options.sheetIndex) && delete options.sheetIndex;
   }
 
-  spawnArgs.push(filename)
+  spawnArgs.push(filename);
 
-  var _data = {}
+  const _data = {};
 
-  var write = fs.createWriteStream(filename)
-    .on('close', function () {
+  function getSheet(childSpawnArgs) {
+    const child = spawn(require.resolve('xlsx/bin/xlsx.njs'), childSpawnArgs);
+
+    child.on('exit', (code) => {
+      if (code === null || code !== 0) {
+        child.stderr.pipe(concat((errstr) => {
+          duplex.emit('error', new Error(errstr));
+        }));
+      }
+    });
+    return child.stdout.pipe(csv.createStream(options));
+  }
+
+  const write = fs.createWriteStream(filename)
+    .on('close', () => {
       if (options.allSheets) {
-        let streams = [];
-        var childCount = spawn(require.resolve('xlsx/bin/xlsx.njs'), ['--list-sheets', filename]);
+        const streams = [];
+        const childCount = spawn(require.resolve('xlsx/bin/xlsx.njs'), ['--list-sheets', filename]);
         childCount.stdout
           .pipe(eventStream.split())
-          .pipe(eventStream.filterSync(sheetName => sheetName))
-          .pipe(eventStream.mapSync((sheetName) => {
-            return getSheet(['--sheet', sheetName, filename]);
-          }))
+          .pipe(eventStream.filterSync((sheetName) => sheetName))
+          .pipe(eventStream.mapSync((sheetName) => getSheet(['--sheet', sheetName, filename])))
           .pipe(eventStream.mapSync((item) => {
             streams.push(item);
             return item;
           }))
           .on('end', () => {
             eventStream.merge(streams)
-              .pipe(through(function (data) {
-                for (var k in data) {
-                  var value = data[k].trim()
-                  _data[k.trim()] = isNaN(value) ? value : +value
-                }
-                this.queue(_data)
+              .pipe(eventStream.through(function (data) {
+                const keys = Object.keys(data);
+                keys.forEach((k) => {
+                  const value = data[k].trim();
+                  _data[k.trim()] = isNaN(value) ? value : +value;
+                });
+                this.queue(_data);
               }))
               .pipe(read);
-          })
+          });
       } else {
-        getSheet(spawnArgs)
+        getSheet(spawnArgs);
       }
-    })
+    });
 
-  function getSheet(childSpawnArgs) {
-    var child = spawn(require.resolve('xlsx/bin/xlsx.njs'), childSpawnArgs)
-
-    child.on('exit', function (code, sig) {
-      if (code === null || code !== 0) {
-        child.stderr.pipe(concat(function (errstr) {
-          duplex.emit('error', new Error(errstr))
-        }))
-      }
-    })
-    return child.stdout.pipe(csv.createStream(options))
-  }
-
-  return (duplex = duplexer(write, read))
-
-}
+  return (duplex = eventStream.duplex(write, read));
+};
 
 
 if (!module.parent) {
-  var JSONStream = require('JSONStream')
-  var args = require('minimist')(process.argv.slice(2))
+  const JSONStream = require('JSONStream');
+  const args = require('minimist')(process.argv.slice(2));
   process.stdin
     .pipe(module.exports())
     .pipe(args.lines || args.newlines
       ? JSONStream.stringify('', '\n', '\n', 0)
-      : JSONStream.stringify()
-    )
-    .pipe(process.stdout)
+      : JSONStream.stringify())
+    .pipe(process.stdout);
 }
